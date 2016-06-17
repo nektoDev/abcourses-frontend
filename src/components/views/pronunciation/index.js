e3c638ea85e0a7ab61d5e0c70085d0246fca9b2f
nektodev <ts.slawa@gmail.com>
2016-06-17 09:00:33 +0300

    Implement cards on pronunciation page

--- src/components/views/pronunciation/index.js
+++ src/components/views/pronunciation/index.js
@@ -1,20 +1,35 @@
 import React from 'react';
+import {Row, Col} from 'react-flexgrid';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
 
 const Pronunciation = React.createClass({
     render: function () {
         return (
             <div>
                 <h1>Pronunciation {this.props.student}</h1>
-                {
-                    this.props.words.map(word => {
-                        return (
-                            <p key={word.id}>{word.word} - {word.pronunciation}</p>
-                        )
-                    })
-                }
+                <Row>
+                    {getCards(this.props.words)}
+                </Row>
             </div>
         );
     }
 });
+
+function getCards(words) {
+
+    return (words.map(word => {
+        return (
+            <Col key={"col-"+word.id} lg={3} sm={12} md={6}>
+                <Card key={"card-"+word.id}>
+                    <CardHeader key={"card-header-"+word.id}
+                                title={word.word}
+                                subtitle={word.pronunciation}
+                    />
+                </Card>
+            </Col>
+        )
+    }))
+}
 
 export default Pronunciation;
