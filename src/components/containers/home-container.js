3a67c18fb2730ecd261d41d15158b90936811ab8
nektodev <ts.slawa@gmail.com>
2016-06-17 09:00:07 +0300

    Add autohiding navigation on main page

--- src/components/containers/home-container.js
+++ src/components/containers/home-container.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import store from '../../store'
 
 //Actions
+import { HIDE_NAV } from '../../actions/action-types'
 
 //View
 import Home from '../views/home';
 
 const HomeContainer = React.createClass({
+
+    componentDidMount: function() {
+        store.dispatch({type: HIDE_NAV});
+    },
 
     render: function() {
         return <Home navigation={this.props.navigation}/>;
