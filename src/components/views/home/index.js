e970eb163f1e2a47d27c8d8b012304fe224c49ee
nektodev <ts.slawa@gmail.com>
2016-07-07 22:27:20 +0300

    Change home page

--- src/components/views/home/index.js
+++ src/components/views/home/index.js
@@ -2,51 +2,71 @@ import React from 'react';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import {Grid, Row, Col} from 'react-flexgrid';
 import {Link} from 'react-router';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
 
 const Home = React.createClass({
     render: function () {
         return (
             <div>
-                {getNavigation(this.props.navigation)}
+                <h1>Alexey Baskinov English Courses</h1>
+                <Row>
+                    <Col lg={9}>
+                        <Row>
+                            <Col lg={6}>
+                                <Link to="/slava/">
+                                    <Card className="card" zDepth={2}>
+                                        <CardMedia overlay={<CardTitle title="Slava"/>}>
+                                            <img src="http://lorempixel.com/600/400/nature/?1"/>
+                                        </CardMedia>
+                                    </Card>
+                                </Link>
+                            </Col>
+                            <Col lg={6}>
+                                <Link to="/daria/">
+                                    <Card className="card" zDepth={2}>
+                                        <CardMedia overlay={<CardTitle title="Daria"/>}>
+                                            <img src="http://lorempixel.com/600/400/nature/?2"/>
+                                        </CardMedia>
+                                    </Card>
+                                </Link>
+                            </Col>
+                        </Row>
+
+                        <Row>
+                            <Col lg={6}>
+                                <Link to="/aydar/">
+                                    <Card className="card" zDepth={2}>
+                                        <CardMedia overlay={<CardTitle title="Aydar"/>}>
+                                            <img src="http://lorempixel.com/600/400/nature/?3"/>
+                                        </CardMedia>
+                                    </Card>
+                                </Link>
+                            </Col>
+                            <Col lg={6}>
+                                <Link to="/yuliya/">
+                                    <Card className="card" zDepth={2}>
+                                        <CardMedia overlay={<CardTitle title="Yuliya"/>}>
+                                            <img src="http://lorempixel.com/600/400/nature/?4"/>
+                                        </CardMedia>
+                                    </Card>
+                                </Link>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col lg={3}>
+                        <h2>Useful links</h2>
+                            <Menu>
+                                <MenuItem primaryText="Oxford dictionary" valueLink="https://www.oxfordlearnersdictionaries.com/"/>
+                                <MenuItem primaryText="Forvo" valueLink="http://ru.forvo.com/" />
+                            </Menu>
+                    </Col>
+
+                </Row>
             </div>
         );
     }
 });
-
-function getNavigation(navigation) {
-    return (
-        navigation.map(sub => {
-            return (
-                <div key={"div-" + sub.id}>
-                    <h2 key={"h2-" + sub.id}>{sub.title}</h2>
-
-                    <Row key={"row-" + sub.id}>
-                        {getPages(sub.pages)}
-                    </Row>
-                </div>
-            )
-        })
-    );
-}
-
-function getPages(pages) {
-    return (
-        pages.map(page => {
-            return(
-                <Col key={"col-" + page.id} lg={4} sm={12} md={12}>
-                    <Link key={"link-" + page.id} to={page.url}>
-                        <Card key={"card-" + page.id} className="card" zDepth={2}>
-                            <CardMedia key={"card-media-" + page.id}
-                                overlay={<CardTitle title={page.title} subtitle={page.subtitle} />}
-                            >
-                                <img key={"img-" + page.id} src={page.picture}/>
-                            </CardMedia>
-                        </Card>
-                    </Link>
-                </Col>
-            )
-        })
-    )
-}
 
 export default Home;
