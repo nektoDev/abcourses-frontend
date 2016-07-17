56224c98d83d62a8cefd901d088a71f1dfbe8e73
nektodev <ts.slawa@gmail.com>
2016-07-17 22:54:54 +0300

    Add navigation to progress

--- src/components/views/student-home/index.js
+++ src/components/views/student-home/index.js
@@ -8,7 +8,7 @@ const StudentHome = React.createClass({
         return (
             <div>
                 <Row>
-                    <Col lg={6} sm={12} md={6}>
+                    <Col lg={4} sm={12} md={4}>
                         <Link to={"/" + this.props.student.id + "/vocabulary"}>
                             <Card className="card" zDepth={2}>
                                 <CardMedia overlay={<CardTitle title="Vocabulary" subtitle="Try to remember all the words!" />}>
@@ -17,10 +17,19 @@ const StudentHome = React.createClass({
                             </Card>
                         </Link>
                     </Col>
-                    <Col lg={6} sm={12} md={6}>
+                    <Col lg={4} sm={12} md={4}>
                         <Link to={"/" + this.props.student.id + "/pronunciation"}>
                             <Card className="card" zDepth={2}>
                                 <CardMedia overlay={<CardTitle title="Pronunciation" subtitle="Put a spoon on your tongue!" />}>
+                                    <img src="http://lorempixel.com/600/400/nature/?2"/>
+                                </CardMedia>
+                            </Card>
+                        </Link>
+                    </Col>
+                    <Col lg={4} sm={12} md={4}>
+                        <Link to={"/" + this.props.student.id + "/progress"}>
+                            <Card className="card" zDepth={2}>
+                                <CardMedia overlay={<CardTitle title="Progress" subtitle="Put a spoon on your tongue!" />}>
                                     <img src="http://lorempixel.com/600/400/nature/?2"/>
                                 </CardMedia>
                             </Card>
