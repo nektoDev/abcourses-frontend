00901e70102985fb25e666b2b855f66a68d33fbb
nektodev <ts.slawa@gmail.com>
2016-08-11 09:15:39 +0300

    Add release script

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+RELEASE=$1
+if [ -z "$RELEASE" ]
+  then
+    $RELEASE=minor
+fi
+npm run release:$RELEASE
+VERSION=`jq '.version' package.json | tr -d '"'`
+echo $VERSION
+docker build --rm -t nektodev/baskinov-frontend:$VERSION -t nektodev/baskinov-frontend .
+docker push nektodev/baskinov-frontend
