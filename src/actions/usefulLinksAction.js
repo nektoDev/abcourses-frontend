af77d25593ee0baac2a7673095b728cf9866d3b0
nektodev <ts.slawa@gmail.com>
2016-10-03 09:52:33 +0300

    Add jsonrpc library and implement getting links by jsonrpc

--- src/actions/usefulLinksAction.js
+++ src/actions/usefulLinksAction.js
@@ -1,6 +1,7 @@
 import {API_LIST_LINKS_SUCCESS} from './const';
 import axios from 'axios';
 let config = require('config');
+var jsonrpc = require('jsonrpc-lite');
 
 function listLinksCreator(parameter) {
   return {type: API_LIST_LINKS_SUCCESS, parameter};
@@ -8,10 +9,14 @@ function listLinksCreator(parameter) {
 
 export function listLinks(dispatch) {
   return () => {
-    axios.get(config.default.apiHost + 'service/usefulLinks')
+    axios.post(config.default.apiHost + 'service', jsonrpc.request('1', 'listUsefulLinks'))
       .then(response => {
-        var action = listLinksCreator(response.data);
-        dispatch(action);
+        var parsed = jsonrpc.parseObject(response.data);
+
+        if (parsed.type == "success") {
+          var action = listLinksCreator(parsed.payload.result);
+          dispatch(action);
+        }
         return action;
       });
   };
